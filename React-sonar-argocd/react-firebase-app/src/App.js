fdc42e2c011733f9f13f86dd97e41808a6806443
gavvala sai kiran <kirangavvala078@gmail.com>
2025-04-15 12:12:19 +0530

    Update App.js4

--- React-sonar-argocd/react-firebase-app/src/App.js
+++ React-sonar-argocd/react-firebase-app/src/App.js
@@ -21,7 +21,7 @@ function App() {
       </header>
 
       <div className="New-container">
-        <p className="New-container-text">Desktop & Mobile Application</p>
+        <p className="New-container-text">Desktop</p>
         <button className="New-container-button" onClick={handleOpenNewTask}>Create Task</button>
       </div>
 
